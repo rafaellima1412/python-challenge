dc9151447e3536596fce1a3e7852b7c67a28549c
rafaellima1412 <rafaellima1412@gmail.com>
2021-08-25 19:03:45 -0400

    model veiculo

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9=mk4o%7!j&y7=)we+e(3jonrmtkh_a7so5!i&rzt3bnz(8^#!'
+SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- parking/models.py
+++ parking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Veiculo(models.Model):
+    placa = models.CharField('placa', max_length=8, null=False)
+    modelo = models.CharField('modelo', max_length=100, null=False)
+    cor = models.CharField('cor', max_length=50, null=False)
+    data_entrada = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.placa
+
